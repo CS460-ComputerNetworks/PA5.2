4d7f7ae6eaac3cc260f63ed79df60895b45aafb3
noschwa <115322172+noschwa@users.noreply.github.com>
2023-11-09 14:46:41 -0700

    Update main_server.h

--- src/server/main_server.h
+++ src/server/main_server.h
@@ -1,1 +1,12 @@
-// temp
+// main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+// Define constants for message types
+#define JOIN 1
+#define LEAVE 2
+#define NOTE 3
+#define SHUTDOWN 4
+#define SHUTDOWN_ALL 5
+
+#endif // MAIN_H
