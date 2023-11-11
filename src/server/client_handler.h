1868918ca91216ecce7326b446d62f6a2cb4ade4
noschwa <115322172+noschwa@users.noreply.github.com>
2023-11-10 18:51:53 -0700

    Update client_handler.h

--- src/server/client_handler.h
+++ src/server/client_handler.h
@@ -2,7 +2,12 @@
 #ifndef CLIENT_HANDLER_H
 #define CLIENT_HANDLER_H
 
-// Function prototype for client handler
-void talk_to_client(void* arg);
+void talk_to_client(void *arg);
+int client_exists(int client_socket);
+void add_client(int client_socket);
+void remove_client(int client_socket);
+char *get_message(int client_socket);
+void send_message(int message_type, int client_socket, char *message);
+void send_message_to_all(int message_type, int client_socket);
 
 #endif // CLIENT_HANDLER_H
